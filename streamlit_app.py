20c8555b704194138e0471a9eacaeab622672e4c
claraa-silva <anaclarasilv1511@gmail.com>
2025-03-14 19:04:04 +0000

    Raízes atualizada

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Meu primeiro app!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Heyyy!"
 )
